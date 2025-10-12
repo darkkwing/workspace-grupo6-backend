57fbfb68892790f893e3cf75a0b03b2544436faf
darkkwing <dargamarr@gmail.com>
2025-10-11 23:42:28 -0300

    darkmode actualizacion
    cambie el login, y le agregue el darkmode, tambien cree un html para registrar, pero esta vacio, retoque estilos, y me falta retocar estilos en sell.html en el darkmode. Hice un pequeño cambio en login.js para poder usar el boton de crear una cuenta.

--- js/login.js
+++ js/login.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => { // Sirve para que no se ejecute el resto del script a menos que el DOM haya terminado de cargar.
     const form = document.querySelector("form"); // Sirve para seleccionar el primer "form" que haya en la pagina.
     const email = document.getElementById("email"); // Sirve para seleccionar el elemento con el id "email" en la pagina.
-    const password = document.getElementById("password") // Sirve para seleccionar el elemento con el id "password" en la pagina.
+    const password = document.getElementById("password"); // Sirve para seleccionar el elemento con el id "password" en la pagina.
+    let registerBtn = document.getElementById("register-btn");
 
     form.addEventListener("submit", (e) => { // Sirve para que cuando se aprete el boton se ejecute el codigo.
         e.preventDefault(); // Sirve para que la pagina no se actualice cuando apretamos el boton. Si no la pagina se actualizaria y no te redirigiria a "index.html".
 
         if (email.value.trim() && password.value.trim()) { // Si los dos campos tienen texto, se redirige al archivo "index.html".
-          sessionStorage.setItem("logueado", "true"); //Objeto que guarda los datos de inicio de sesión
+            sessionStorage.setItem("logueado", "true"); //Objeto que guarda los datos de inicio de sesión
 
-          const username = email.value;           //Se crea una variable donde se almacena el valor del input email
-          sessionStorage.setItem("usuario", username);   //Objeto que guarda los datos de la variable
+            const username = email.value;           //Se crea una variable donde se almacena el valor del input email
+            sessionStorage.setItem("usuario", username);   //Objeto que guarda los datos de la variable
 
-          window.location.href = "index.html"; // Determina a que archivo se redirige si ambos campos tienen texto
+            window.location.href = "index.html"; // Determina a que archivo se redirige si ambos campos tienen texto
         } else { // Si alguno de los dos campos esta vacio se muestra una alerta al usuario.
             alert("Ingresa tu correo electrónico y contraseña."); // Alerta.
         }
     });
+
+    if (registerBtn) {
+        registerBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            window.location.href = "register.html";
+        });
+    }
 });
