4cc00e4135b08615317dfbbbc3e25e9e8fd4f442
micaela-perez304 <micaela.perez3848@gmail.com>
2025-11-25 18:28:29 -0300

    si esta bien es un milagro

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "el_groupe_six_es_el_mejor";  // Misma clave que usamos en  el archivo auth.js
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token no proporcionado" });
+  }
+
+  const parts = authHeader.split(" ");
+
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res.status(401).json({ message: "Formato de token inválido" });
+  }
+
+  const token = parts[1];
+
+  jwt.verify(token, SECRET_KEY, (err, decodedUser) => {
+    if (err) {
+      return res.status(403).json({ message: "Token inválido o expirado" });
+    }
+
+    req.user = decodedUser;
+    next();
+  });
+};
+
+module.exports = { authMiddleware };
